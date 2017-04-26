51c9677e2075d111cee7acaef302b99204c75101
Cody-Joe Abe <cabe@cs.uoregon.edu>
2017-04-25 17:27:55 -0700

    Finished functionality and now we can clean up the code.

--- DuckDuckGroup.py
+++ DuckDuckGroup.py
@@ -18,22 +18,37 @@ def groups():
         reader_python = sys.argv[5]
         csv_file = sys.argv[6]
         categories_txt = sys.argv[7]
+        output_txt = sys.argv[8]
+
 
         subprocess.run(['python3', categories_python, csv_file, '>', categories_txt], stdout=subprocess.PIPE).stdout.decode('utf-8')
-        print(categories_txt)
+        subprocess.run(['python3', reader_python, csv_file, '>', output_txt], stdout=subprocess.PIPE).stdout.decode('utf-8')
+        subprocess.check_call(['javac', 'person.java', 'grouping.java'], stdout=subprocess.PIPE)
+        subprocess.run(['java', 'grouping', output_txt, 'final.txt'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        print('successfully ran java')
         #return str(result)
         #return render_template('app.html')
     #if request.method == 'POST':
         # for column in CSV
         #if request.form["button pressed"] == "displaySliders":
         criteria_list = []
+        groups = []
         categories = open(categories_txt, "r")
+        print(categories)
         for item in categories:
             criteria_list.append(item)
 
+        groupings = open("final.txt", "r")
+        i = 1
+        for group in groupings:
+
+            groups.append([i,group])
+            i+=1
+
+
         session['num_criteria'] = criteria_list
         session['criteria_count'] = len(criteria_list)
-        session['groups'] = [[1, "Cody Abe"], [2, "Randy Hen"], [3, "Jeremy Lin"]]
+        session['groups'] = groups
 
         #return redirect(url_for('groups'))
         #if request.form["button pressed"] == "displayTeams":

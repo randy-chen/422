c71fd32609f0f2d5a9d15e3020deb65ae79c27fc
Cody-Joe Abe <cabe@cs.uoregon.edu>
2017-04-26 19:16:19 -0700

    added comments to files, changed the testCSV to final version

--- utils/CSVReader.py
+++ utils/CSVReader.py
@@ -8,15 +8,28 @@ Copyright © 2017 Cody Abe. All rights reserved.
 import csv
 import sys
 
+
+
 def read_csv(csv_input):
+    '''
+    Usage: To create an array of students that will be parsed later.
+    arg: Input in the form of a csv file
+    returns: an array of students
+    '''
     with open(csv_input, 'r') as csvfile:
-        students = csv.reader(csvfile, delimiter=',', quotechar='"')
+        students = csv.reader(csvfile, delimiter=',', quotechar='"')  #creates correct format of csv
         array = []
         for row in students:
             array.append(row) #putting array into another array (just to print out at the end)
         return array
 
 def parse_categories(csv_array):
+    '''
+    Usage: To create an array of categories in order to be output into the final
+    text file.
+    arg: the array from the read_csv function
+    returns: a dictionary of categories
+    '''
     categories_array = csv_array.pop(0)
     categories_dict = {}
     for i in range(len(categories_array)):
@@ -24,13 +37,20 @@ def parse_categories(csv_array):
     return categories_dict
 
 def parse_student(student, categories_dict):
+    '''
+    Usage: To create an array of students that will be parsed later.
+    arg:
+        student: This is the array after parse_categories
+        categories_dict: This is the dictionary of categories from parse_categories
+    returns: a string of students
+    '''
     count = len(categories_dict)
     student_string = ''
     for i in range(count):
-        if(i == 0):
+        if(i == 0): #if its the name so the first and last name can be on the same line
             student_string += student[i]
             student_string += ' '
-        else:
+        else: #puts the categories on new lines
             student_string += student[i]
             student_string += '\n'
     return student_string
